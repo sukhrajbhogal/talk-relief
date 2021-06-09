00a09355611215f54fd3f92c197d6c6586f4c2bd
Sukhraj Bhogal <saibibhogal1@gmail.com>
2021-06-09 15:08:20 -0400

    fixed initial auth navigation

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useEffect, navRef } from "react";
 import { useSelector } from "react-redux";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  NavigationActions,
+} from "@react-navigation/native";
 
 import HomeNavigation from "./HomeNavigation";
 //import { HomeNavigation, AuthNavigation } from "./HomeNavigation";
 //import StartupScreen from '../screens/StartupScreen';
 
 const AppNavigation = (props) => {
-  //const isAuth = useSelector((state) => !!state.auth.token);
-  //const didTryAutoLogin = useSelector(state => state.auth.didTryAutoLogin);
+  const isAuth = useSelector((state) => !!state.auth.token);
+  const navRef = useRef();
 
-  return (
-    <NavigationContainer>
-      <HomeNavigation />
-      {/* {isAuth && <HomeNavigation />} */}
-      {/* {!isAuth && didTryAutoLogin && <AuthNavigation />} */}
-      {/* {!isAuth && !didTryAutoLogin && <StartupScreen />} */}
-    </NavigationContainer>
-  );
+  useEffect(() => {
+    if (!isAuth) {
+      navRef.current.dispatch(
+        NavigationsActons.navigate({ routeName: "Auth" })
+      );
+    }
+  }, [isAuth]);
+
+  return <HomeNavigation ref={navRef} />;
 };
 
 export default AppNavigation;
