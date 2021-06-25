8f3df70b697ac00489a3c5e46de8f8dc52b64b8b
dvdngn <david.tai.nn@gmail.com>
2021-06-24 20:32:56 -0400

    Updated Onboarding Screen

--- models/OnboardingSlides.js
+++ models/OnboardingSlides.js
@@ -1,30 +1,30 @@
 export default [
   {
     id: "1",
-    title: "",
+    title: "Welcome to TalkRelief",
     description:
-      "Click on the house icon to scroll through various messages from other users, and then provide some advice/support for the messages you want to reply to.",
+      "Users share their feelings and get support from others going through similar experiences. Tap on a post to reply with comfort, sympathy, or advice.",
     image: require("../assets/O1.png"),
   },
   {
     id: "2",
-    title: "",
+    title: "Express your feelings",
     description:
-      "Press the plus icon to submit a message and then press send to send out your message to your feed. Others will reply to your messages promptly.",
+      "You can create a new post by tapping on the ⨁ icon. Users can read your post and reply with support, encouragement, or advice.",
     image: require("../assets/O2.png"),
   },
   {
     id: "3",
-    title: "",
+    title: "Receive comforting words",
     description:
-      "Type your response by clicking the pencil button below, and then submit your response by pressing “send”.",
+      "After you create a post, you can view responses from the community by clicking on the mail icon.",
     image: require("../assets/O3.png"),
   },
   {
     id: "4",
-    title: "",
+    title: "Be respectful",
     description:
-      "Click the envelope button to see the responses which fellow users have written for you about your concern.",
-    image: require("../assets/O3.png"),
+      "This is a place of support for real people with real problems. Violators will be banned from the community.",
+    image: require("../assets/O4.png"),
   },
 ];
